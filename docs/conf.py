3fc2219ba12db126775ff731f3ef1a28f090e736
Sep Dehpour <sep@zepworks.com>
2026-03-10 23:34:29 -0700

    Bump version: 8.6.1 → 8.7.0

--- docs/conf.py
+++ docs/conf.py
@@ -64,9 +64,9 @@ author = 'Sep Dehpour'
 # built documents.
 #
 # The short X.Y version.
-version = '8.6.1'
+version = '8.7.0'
 # The full version, including alpha/beta/rc tags.
-release = '8.6.1'
+release = '8.7.0'
 
 load_dotenv(override=True)
 DOC_VERSION = os.environ.get('DOC_VERSION', version)
